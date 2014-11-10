3dec9b24f3ba3e13e45979d94a417c84374eb914
Johannes Harkins <johannes.harkins@gmail.com>
2014-11-09 19:39:57 -0800

    Angular Filter working on lines

--- plxplor.js
+++ plxplor.js
@@ -63,59 +63,64 @@ plApp.directive("parallel", function() {
 							   .x(function(d){ return d[0]})
 							   .y(function(d){ return d[1]})
 							   .interpolate("linear")
-			var series = data.map(function(d) { 
-				return {
-					"club": d.Club,
-					"playername": d["Player Name"],
-					"position": d.Position,
-					"stats": d3.keys(d).filter(function(k){ return ["Club","Player Name", "Position"].indexOf(k) == -1 })
-							   .map(function(key) {
-							   	return { 
-							   		"cat": key,
-							   		"val": +d[key]
-							   	}
-							   })
 
-				}
-			})
+			scope.$watchCollection('idata()', function(newData, oldData) { 	
+				var series = newData.map(function(d) { 
+					return {
+						"club": d.Club,
+						"playername": d["Player Name"],
+						"position": d.Position,
+						"stats": d3.keys(d).filter(function(k){ return ["Club","Player Name", "Position"].indexOf(k) == -1 })
+								   .map(function(key) {
+								   	return { 
+								   		"cat": key,
+								   		"val": +d[key]
+								   	}
+								   })
 
-			var xscale = d3.scale.linear()
-						   .domain([0,series[0].stats.length])
-						   .range([0, width])
-
-			var maxArray = series[0].stats.map(function(stat) {
-				return {
-					"stat": stat.cat,
-					"minval": d3.min(series, function(d) {
-						var obj = d.stats.filter(function(s) { return s.cat == stat.cat})
-						return obj[0].val
-						}),
-					"maxval": d3.max(series, function(d) {
-						var obj = d.stats.filter(function(s) { return s.cat == stat.cat})
-						return obj[0].val
-						}) 
 					}
 				})
-			var  y = {}
-			maxArray.forEach(function(item) { 
-				y[item.stat] = d3.scale.linear().domain([item.minval,item.maxval]).range([height,0]);
+
+				var xscale = d3.scale.linear()
+							   .domain([0,series[0].stats.length])
+							   .range([0, width])
+
+				var maxArray = series[0].stats.map(function(stat) {
+					return {
+						"stat": stat.cat,
+						"minval": d3.min(series, function(d) {
+							var obj = d.stats.filter(function(s) { return s.cat == stat.cat})
+							return obj[0].val
+							}),
+						"maxval": d3.max(series, function(d) {
+							var obj = d.stats.filter(function(s) { return s.cat == stat.cat})
+							return obj[0].val
+							}) 
+						}
+					})
+				var  y = {}
+				maxArray.forEach(function(item) { 
+					y[item.stat] = d3.scale.linear().domain([item.minval,item.maxval]).range([height,0]);
+				})		
+				var lines = svg.selectAll(".pline")
+							   .data(series)
+							
+							lines.enter()
+							     .append("path")
+							     .attr("class", "pline")
+							     .attr("d", pathfn)
+							     .attr("stroke", "steelblue")
+							     .attr("stroke-width", 1.5)
+							     .attr("stroke-opacity", 0.25)
+							     .attr("fill", "none")
+
+				var linesExit = lines.exit().remove()
+
+
+				function pathfn(d)  {
+					 return linefn(d.stats.map(function(s,i) { return [xscale(i), y[s.cat](s.val)]; }))
+				}
 			})
-
-			var lines = svg.append("g")
-						   .attr("class", "pline")
-						   .selectAll("path")
-						   .data(series)
-						   .enter()
-						   .append("path")
-						   .attr("d", pathfn)
-						   .attr("stroke", "steelblue")
-						   .attr("stroke-width", 1.5)
-						   .attr("stroke-opacity", 0.25)
-						   .attr("fill", "none")
-
-			function pathfn(d)  {
-				 return linefn(d.stats.map(function(s,i) { return [xscale(i), y[s.cat](s.val)]; }))
-			}
 
 		}
 	}
