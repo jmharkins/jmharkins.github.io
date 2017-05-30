9e6bc553afa61be5e3e0a331d33d12c0698bc23d
jmharkins <johannes.harkins@gmail.com>
2017-05-29 21:17:45 -0400

    added nhl project

--- nhl.js
+++ nhl.js
@@ -5,34 +5,37 @@ var w = 1100;
      var svg = d3.select("#chart")
          .append("svg")
          .attr("width", w)
-         .attr("height", h);    
+         .attr("height", h);
         // load data
+	var svgBox = d3.select('#chart')[0][0].getBoundingClientRect()
+	var mouseOrigin = [svgBox.left, svgBox.top];
+
  d3.json("nhl.json", function(error, data) {
 	if (error) {  //If error is not null, something went wrong.
-       console.log(error);  
+       console.log(error);
 	}
 	else {
-	// declare variables		
+	// declare variables
 	   var ndata = data;
 	   var gdata;
 	   var stdata = [];
 	   var maxlength = 0;
 	   var maxteam = "";
 	   // sort teams in ascending order of # of goalies
-	   var sortteams = ndata.sort(function(a,b){ 
-    var x = a.goalies.length < b.goalies.length? -1:1; 
-    return x; 
+	   var sortteams = ndata.sort(function(a,b){
+    var x = a.goalies.length < b.goalies.length? -1:1;
+    return x;
     });
     //define y scale
     var yScale = d3.scale.ordinal()
                          .domain(d3.range(ndata.length))
-                         .rangeRoundBands([0,h], .15);                                           
-	//    
+                         .rangeRoundBands([0,h], .15);
+	//
 	// key fns
 	var team = function(d) {
 	   return d.team;
 	};
-	
+
 	var goalies = function(d) {
 		return d.goalies;
 	};
@@ -48,26 +51,26 @@ var w = 1100;
 	   .attr("font-family","sans-serif")
 	   .attr("font-size","11px")
 	   .attr("y", function(d,i) {
-	   	   return yScale(i) + (yScale.rangeBand() /2) + 3; 
+	   	   return yScale(i) + (yScale.rangeBand() /2) + 3;
 	   })
 	   .text(function(d) {
 	   	return d.team
-	   }); 
+	   });
 	// data bind + rectangles
 	for (i = 0; i< ndata.length; i ++){
-		gdata = sortteams[i].goalies.sort(function(a,b){ 
-    var x = a.gp > b.gp? -1:1; 
-    return x; 
+		gdata = sortteams[i].goalies.sort(function(a,b){
+    var x = a.gp > b.gp? -1:1;
+    return x;
     });
     // fix y pos
 		var fixy = yScale(i);
-		
+
 	    var gmax = 0;
 		stdata = [];
 		//
 		for (j = 0; j < gdata.length; j++){
 			// determine total number of games
-		    gmax = gmax + gdata[j].gp	
+		    gmax = gmax + gdata[j].gp
 		    // give data x, y vals
 		    xval = j;
 		    yval = gdata[j].gp;
@@ -77,18 +80,18 @@ var w = 1100;
 		// scale determining bar width
 		var wScale = d3.scale.linear()
 		                     .domain([0,gmax])
-		                     .range([0, barw]);                        
+		                     .range([0, barw]);
         //stack players within team
         var stack = d3.layout.stack();
-        stack(stdata);                                           		                                      
+        stack(stdata);
 		//class label for rectangles belonging to team
 		var classlbl = ".b" + [i]
-		
-		// 
+
+		//
 		var groups = svg.selectAll(classlbl)
                         .data(stdata)
                         .enter()
-                        .append("g")	
+                        .append("g")
 		// add rectangles
  var rect = groups.selectAll("rect")
 	           .data(function(d) {return d;})
@@ -115,8 +118,8 @@ var w = 1100;
 	             // change color
 	           	d3.select(this)
 	           	  .attr("fill","orange");
-	           	var xPosition = parseFloat(d3.select(this).attr("x"));
-				var yPosition = parseFloat(d3.select(this).attr("y")) + 100;
+       	var xPosition = parseFloat(d3.select(this).attr("x"));
+				var yPosition = parseFloat(d3.select(this).attr("y")) + 215;
 				var glabel = d3.select(this).attr("pid");
 				var plabel = d3.select(this).attr("gpid");
 				//Update the tooltip position and value
@@ -142,10 +145,10 @@ var w = 1100;
 		}
 	  }
 	});
-	           
+
 	   ///
-	 /// tooltips 
-// $(document).ready(function() {         
+	 /// tooltips
+// $(document).ready(function() {
 // $("rect").tooltip({
 // 	delay: { show: 250, hide: 250 }
 // 	placement: function(tip, element) {
@@ -161,8 +164,8 @@ var w = 1100;
 // 		return $(element).pid + ": " + $(element).gpid + " Games";
 // 	}
 // 	return this.getAttribute("pid") + ": " + this.getAttribute("gpid") + " Games";
-// 	});	
-	
+// 	});
+
 
 // 	}
 // 	});
